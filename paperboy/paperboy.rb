5c813fa583534b7f679ea02c5ef1799e8097a011
Courtney Noonan <courtneynoonan@me.com>
2016-06-02 20:24:37 -0400

    worked on methods, earnings not updating yet

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,6 +1,6 @@
 class Paperboy
 
-  attr_reader :earnings
+  attr_reader :earnings #must be read only.  How do you update earnings then?
 
   attr_accessor :name, :quota, :experience, :side, :earnings
 
@@ -12,17 +12,30 @@ class Paperboy
     @earnings = earnings
   end
 
-  def quota #half experience + 50 (the minimum)
-
+  def quota
+    50 + experience/2
   end
 
-  def deliver(start_address, end_address) #takes 2 house numbers and return the
-                                        #money earned. $0.25 per paper, and 0.50 each after quota
-                                        #-2.00 if quota isnt method
-                                        #should also update experience
+  def deliver(start_address, end_address) # ----- number of houses !!!! adjust for even or odd side
+
+      houses = end_address - start_address
+      puts houses
+      over_quota = houses - quota
+      puts over_quota
+      @experience = @experience + houses
+
+        if houses > quota
+          total = 0.25 * quota  + 0.50 * over_quota #this line does not work!!!
+          puts total
+        end
+
+        if houses < quota
+          @earnings = @earnings - 2.00
+        end
   end
 
   def report
+    "I'm #{name.capitalize}. I've delivered #{experience} papers and I've earned $#{earnings} so far!!"
 
   end
 
